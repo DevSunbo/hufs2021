80b3c33d57c025d2843d725bfc2ff2fe4de37f57
KIMCOYG <kjhkjh9753@gmail.com>
2021-01-05 19:18:59 +0900

    엔티티 작성

---
+++ src/main/java/hufs2021/jeongbo/model/entity/User.java
@@ -0,0 +1,38 @@
+package hufs2021.jeongbo.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int studentId;
+
+    private String name;
+
+    private String phoneNumber;
+
+    private String email;
+
+    private LocalDateTime createdAt;
+
+    private int createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private int updatedBy;
+}
