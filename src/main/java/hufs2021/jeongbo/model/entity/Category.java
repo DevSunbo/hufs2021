583ca5f209c61bdee9d33757a791d9a944fa5fd2
DevSunbo <cowwhite1008@gmail.com>
2021-01-05 19:22:14 +0900

    Category 기초 완성

--- src/main/java/hufs2021/jeongbo/model/entity/Category.java
+++ src/main/java/hufs2021/jeongbo/model/entity/Category.java
@@ -1,2 +1,30 @@
-package hufs2021.jeongbo.model.entity;public class Category {
+package hufs2021.jeongbo.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer caNumber; // 카테고리 구분 번호 PK
+
+    private Integer caRental; // Enum 대여가능 여부
+    private Integer caKind; // Enum 기자재 종류
+    private Integer caName; // Enum 기자재 이름
+    
+    private LocalDateTime createdAt; // 등록일
+    private Integer createdBy; // 등록자
+    private LocalDateTime updatedAt; // 수정일
+    private Integer updatedBy; // 수정자
 }
