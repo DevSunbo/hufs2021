724d080ff8880debba3fef10e7b2168eb15c48d0
DevSunbo <cowwhite1008@gmail.com>
2021-01-06 00:01:17 +0900

    test passed

--- src/test/java/hufs2021/jeongbo/repository/AssetInventoryRepositoryTest.java
+++ src/test/java/hufs2021/jeongbo/repository/AssetInventoryRepositoryTest.java
@@ -4,13 +4,11 @@ import hufs2021.jeongbo.JeongboApplicationTests;
 import hufs2021.jeongbo.model.entity.AssetInventory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@WebAppConfiguration
 class AssetInventoryRepositoryTest extends JeongboApplicationTests {
     @Autowired
     private AssetInventoryRepository assetInventoryRepository;
