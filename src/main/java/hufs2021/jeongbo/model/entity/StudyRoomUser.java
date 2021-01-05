50ceba2c6e53787da8f724ec5d697c557de58898
DevSunbo <cowwhite1008@gmail.com>
2021-01-05 19:52:16 +0900

    Study Room User 기초 완성

--- src/main/java/hufs2021/jeongbo/model/entity/StudyRoomUser.java
+++ src/main/java/hufs2021/jeongbo/model/entity/StudyRoomUser.java
@@ -8,24 +8,26 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class StudyRoomHistory {
+public class StudyRoomUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer rhNumber; // 구분 번호 PK
+    private Integer ruKey; // 구분 PK
 
-    private LocalDate rhDate; // 예약한 날짜
-    private LocalDateTime rhFrom; // 예약 시작시간
-    private LocalDateTime rhEnd; // 예약 끝시간
+    private Integer ruId; // 호실 번호
+    private Integer ruNumber; // 스터디룸 안 방번호
+    private Integer ruStudentId; // 예약자 학번
+    private Integer ruApplicant; // 예약자 일행 학번
 
     private LocalDateTime createdAt; // 등록일
-    private Integer createdBy; // 등록자 = 예약한 사람 학번번
-   private LocalDateTime updatedAt; // 수정일
+    private Integer createdBy; // 등록자 = 예약한 사람 학번
+    private LocalDateTime updatedAt; // 수정일
     private Integer updatedBy; // 수정자
+
+
 }
