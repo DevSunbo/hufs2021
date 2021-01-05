8e912e283b2ec77a43389f0ec78a23aecc7a385f
DevSunbo <cowwhite1008@gmail.com>
2021-01-05 19:16:17 +0900

    Asset Inventory 기초 완성

--- src/main/java/hufs2021/jeongbo/model/entity/AssetInventory.java
+++ src/main/java/hufs2021/jeongbo/model/entity/AssetInventory.java
@@ -1,2 +1,35 @@
-package hufs2021.jeongbo.model.entity;public class AssetInventory {
+package hufs2021.jeongbo.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class AssetInventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int aiNumber; // 기자재 구분 번호 PK
+
+    private int aiSerial; // 기자재 바코드
+    private LocalDate aiExpiration; // 기자재 만료일
+    private Integer aiStatus; // 기자재 상태
+    private Integer aiRoom; // 기자재 위치 Location room_location Fk
+    private Integer aiUser; // 기자재를 빌린 사람
+    private Integer caNumber; // 기자재 카테고리 CATEGORY ca_number FK
+
+    private LocalDateTime createdAt; // 등록일
+    private Integer createdBy; // 등록자
+    private LocalDateTime updatedAt; // 수정일
+    private Integer updatedBy; // 수정자
+
 }
