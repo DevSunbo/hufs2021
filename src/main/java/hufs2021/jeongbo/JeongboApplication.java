719184f891fc35a911b377d40b6ff8efc074a39f
DevSunbo <cowwhite1008@gmail.com>
2021-01-05 15:54:10 +0900

    Springboot gradle

---
+++ src/main/java/hufs2021/jeongbo/JeongboApplication.java
@@ -0,0 +1,13 @@
+package hufs2021.jeongbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeongboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeongboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hufs2021/jeongbo/JeongboApplicationTests.java
@@ -0,0 +1,13 @@
+package hufs2021.jeongbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeongboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
