306b2851cefc6c9928f32e1c7d49fcd4f719c431
KIMCOYG <kjhkjh9753@gmail.com>
2021-01-09 20:00:16 +0900

    controller team-project read, readall

--- src/main/java/hufs2021/jeongbo/controller/api/TeamProjectController.java
+++ src/main/java/hufs2021/jeongbo/controller/api/TeamProjectController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/team-project")
@@ -19,7 +20,7 @@ public class TeamProjectController {
 
     @PostMapping("/create")
     @ResponseBody
-    public Header<TeamProjectRequest> create(@RequestBody TeamProjectRequest teamProjectRequest) {
+    public Header<TeamProjectResponse> create(@RequestBody TeamProjectRequest teamProjectRequest) {
         TeamProject teamProject = TeamProject.builder()
                 .pField(teamProjectRequest.getPField())
                 .pName(teamProjectRequest.getPName())
@@ -43,6 +44,60 @@ public class TeamProjectController {
             return Header.ERROR();
 
 //        return teamProjectRequest;
+    }
+
+    @GetMapping("/readall")
+    @ResponseBody
+    public Header<TeamProjectResponse> readAll() {
+        List<TeamProject> teamProjectList = teamProjectRepository.findAll();
+
+        if (teamProjectList != null) {
+            teamProjectList.stream().forEach(teamProject -> {
+                System.out.println(teamProject.getPNumber());
+                System.out.println(teamProject.getPName());
+                System.out.println(teamProject.getPMin());
+                System.out.println(teamProject.getPMax());
+                System.out.println(teamProject.getPDeadline());
+                System.out.println(teamProject.getPMainLanguage());
+                System.out.println(teamProject.getPLocation());
+                System.out.println(teamProject.getPContent());
+                System.out.println(teamProject.getFId());
+                System.out.println(teamProject.getCreatedAt());
+                System.out.println(teamProject.getCreatedBy());
+                System.out.println(teamProject.getUpdatedAt());
+                System.out.println(teamProject.getUpdatedBy());
+                System.out.println(teamProject.getStudentId());
+                System.out.println("----------------------------------");
+            });
+            return Header.OK();
+        }
+        else
+            return Header.ERROR();
+    }
+
+    @GetMapping("/read")
+    @ResponseBody
+    public Header<TeamProjectResponse> read(@RequestParam Integer number) {
+        return teamProjectRepository.findById(number)
+                .map(teamProject -> {
+                    System.out.println(teamProject.getPNumber());
+                    System.out.println(teamProject.getPName());
+                    System.out.println(teamProject.getPMin());
+                    System.out.println(teamProject.getPMax());
+                    System.out.println(teamProject.getPDeadline());
+                    System.out.println(teamProject.getPMainLanguage());
+                    System.out.println(teamProject.getPLocation());
+                    System.out.println(teamProject.getPContent());
+                    System.out.println(teamProject.getFId());
+                    System.out.println(teamProject.getCreatedAt());
+                    System.out.println(teamProject.getCreatedBy());
+                    System.out.println(teamProject.getUpdatedAt());
+                    System.out.println(teamProject.getUpdatedBy());
+                    System.out.println(teamProject.getStudentId());
+                    return teamProject;
+                }).map(teamProject -> Header.OK(response(teamProject)))
+                .orElseGet(Header::ERROR);
+
     }
 
     private TeamProjectResponse response(TeamProject teamProject) {
