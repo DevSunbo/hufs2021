5dbce76d0dda883b721b12ffeaaf40a4eeaadfcc
KIMCOYG <kjhkjh9753@gmail.com>
2021-01-14 17:49:06 +0900

    fileService create, read, readall, update, delete

--- src/main/java/hufs2021/jeongbo/service/FileService.java
+++ src/main/java/hufs2021/jeongbo/service/FileService.java
@@ -1,31 +1,25 @@
-package hufs2021.jeongbo.controller.api;
+package hufs2021.jeongbo.service;
 
 import hufs2021.jeongbo.model.entity.File;
 import hufs2021.jeongbo.network.Header;
 import hufs2021.jeongbo.network.request.FileRequest;
 import hufs2021.jeongbo.network.response.FileResponse;
-import hufs2021.jeongbo.service.FileService;
+import hufs2021.jeongbo.repository.FileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
 
-@RestController
-@RequestMapping(value = "/api/file", produces = "application/json")
-public class FileController {
-
-//    @Autowired
-//    private FileRepository fileRepository;
+@Service
+public class FileService {
 
     @Autowired
-    private FileService fileService;
+    private FileRepository fileRepository;
 
-    @PostMapping("/create")
-    @ResponseBody
-    public Header<FileResponse> create(@RequestBody FileRequest fileRequest) {
+    public Header<FileResponse> create(FileRequest fileRequest) {
 
-        /*File file = File.builder()
+        File file = File.builder()
                 .fName(fileRequest.getFName())
                 .createdAt(LocalDateTime.now())
                 .createdBy(1234)
@@ -36,14 +30,11 @@ public class FileController {
         if(newFile!=null)
             return Header.OK(response(newFile));
         else
-            return Header.ERROR();*/
-        return fileService.create(fileRequest);
+            return Header.ERROR();
     }
 
-    @GetMapping("/readall")
-    @ResponseBody
     public Header<FileResponse> readAll() {
-        /*List<File> fileList = fileRepository.findAll();
+        List<File> fileList = fileRepository.findAll();
 
         if(fileList!=null){
             fileList.stream().forEach(file -> {
@@ -55,45 +46,37 @@ public class FileController {
             });
             return Header.OK();
         }
-        return Header.ERROR();*/
-        return fileService.readAll();
+        return Header.ERROR();
     }
 
-    @GetMapping("/read")
-    public Header<FileResponse> read(@RequestParam Integer id) {
-        /*return fileRepository.findById(id).map(file -> {
+    public Header<FileResponse> read(Integer id) {
+        return fileRepository.findById(id).map(file -> {
             System.out.println("파일 아디: " + file.getFId());
             System.out.println("파일 명: " + file.getFName());
             System.out.println("등록일: " + file.getCreatedAt());
             System.out.println("등록자: " + file.getCreatedBy());
             System.out.println("---------------");
             return Header.OK(response(file));
-        }).orElseGet(Header::ERROR);*/
-        return fileService.read(id);
+        }).orElseGet(Header::ERROR);
     }
 
-    @PutMapping("/update")
-    @ResponseBody
-    public Header<FileResponse> update(@RequestBody FileRequest fileRequest) {
+    public Header<FileResponse> update(FileRequest fileRequest) {
 
-        /*return fileRepository.findById(fileRequest.getFId()).map(file -> {
+        return fileRepository.findById(fileRequest.getFId()).map(file -> {
             file.setFName(fileRequest.getFName());
             file.setUpdatedAt(LocalDateTime.now());
             file.setUpdatedBy(fileRequest.getUpdatedBy());
 
             File updatedFile = fileRepository.save(file);
             return Header.OK(response(updatedFile));
-        }).orElseGet(Header::ERROR);*/
-        return fileService.update(fileRequest);
+        }).orElseGet(Header::ERROR);
     }
 
-    @DeleteMapping("/delete")
-    public Header<FileResponse> delete(@RequestParam Integer id){
-        /*return fileRepository.findById(id).map(file -> {
+    public Header<FileResponse> delete(Integer id){
+        return fileRepository.findById(id).map(file -> {
             fileRepository.delete(file);
             return Header.OK(response(file));
-        }).orElseGet(Header::ERROR);*/
-        return fileService.delete(id);
+        }).orElseGet(Header::ERROR);
     }
 
     private FileResponse response(File file) {
@@ -106,25 +89,5 @@ public class FileController {
                 .updatedBy(file.getUpdatedBy())
                 .build();
     }
-
-    private FileResponse response(List<File> fileList) { //안됨
-        List<FileResponse> fileResponseList = new ArrayList<>();
-
-        fileList.stream().forEach(file -> {
-            FileResponse fileResponse = FileResponse.builder()
-                    .fId(file.getFId())
-                    .fName(file.getFName())
-                    .createdAt(file.getCreatedAt())
-                    .createdBy(file.getCreatedBy())
-                    .updatedAt(file.getUpdatedAt())
-                    .updatedBy(file.getUpdatedBy())
-                    .build();
-
-            fileResponseList.add(fileResponse);
-        });
-
-        return (FileResponse) fileResponseList;
-    }
-
 
 }
