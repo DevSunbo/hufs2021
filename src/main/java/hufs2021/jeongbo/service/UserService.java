47a528bb75961cc167252b41bd4284174a900786
DevSunbo <cowwhite1008@gmail.com>
2021-01-16 23:28:24 +0900

    UserSevice delete, make later

--- src/main/java/hufs2021/jeongbo/service/UserService.java
+++ src/main/java/hufs2021/jeongbo/service/UserService.java
@@ -1,2 +1,20 @@
-package hufs2021.jeongbo.service;public class UserService {
+package hufs2021.jeongbo.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+/*
+
+@Data
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+   */
+/* @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    }*//*
+
 }
+*/
