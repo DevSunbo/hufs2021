0558ff72dccf839a1f44712673fef846a696bdc5
DevSunbo <cowwhite1008@gmail.com>
2021-01-21 02:30:41 +0900

    signup controller

--- src/main/java/hufs2021/jeongbo/controller/login/SignupController.java
+++ src/main/java/hufs2021/jeongbo/controller/login/SignupController.java
@@ -2,17 +2,48 @@ package hufs2021.jeongbo.controller.login;
 
 import hufs2021.jeongbo.model.entity.User;
 import hufs2021.jeongbo.model.network.Header;
+import hufs2021.jeongbo.network.request.UserRequest;
+import hufs2021.jeongbo.network.response.UserResponse;
+import hufs2021.jeongbo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping("/signup")
 public class SignupController {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @PostMapping("/")
-    public Header signup(@RequestBody User user){
-        return Header.OK();
+    public Header<UserResponse> signup(@RequestBody UserRequest userRequest){
+        String encodedPassword = passwordEncoder.encode(userRequest.getPassword());  //BCryptPasswordEncoder
+
+        User user = User.builder()
+                .studentId(userRequest.getStudentId())
+                .name(userRequest.getName())
+                .phoneNumber(userRequest.getPhoneNumber())
+                .email(userRequest.getEmail())
+                .password(encodedPassword)
+                .createdAt(LocalDateTime.now())
+                .createdBy(userRequest.getCreatedBy())
+                .mCode(userRequest.getMCode())
+                .build();
+
+        User newUser = userRepository.save(user);
+
+        if(newUser!=null)
+            return Header.OK();
+        else
+            return Header.ERROR("error");
     }
 }
