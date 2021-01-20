9de59f12cd11d8daeffcd907b122a42605299f2a
DevSunbo <cowwhite1008@gmail.com>
2021-01-21 02:41:12 +0900

    don't use

--- src/main/java/hufs2021/jeongbo/controller/login/LoginController.java
+++ src/main/java/hufs2021/jeongbo/controller/login/LoginController.java
@@ -1,7 +1,12 @@
 package hufs2021.jeongbo.controller.login;
 
+import hufs2021.jeongbo.model.network.Header;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/login")
