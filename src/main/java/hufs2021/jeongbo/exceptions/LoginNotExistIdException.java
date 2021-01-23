a2f0f88aca90346cc57a8dcc22274a53ccee25a0
DevSunbo <cowwhite1008@gmail.com>
2021-01-23 17:49:02 +0900

    make exception

--- src/main/java/hufs2021/jeongbo/exceptions/LoginNotExistIdException.java
+++ src/main/java/hufs2021/jeongbo/exceptions/LoginNotExistIdException.java
@@ -1,2 +1,17 @@
-package hufs2021.jeongbo.exceptions;public class LoginNotExistIdException {
+package hufs2021.jeongbo.exceptions;
+
+public class LoginNotExistIdException extends Exception{
+
+
+    public LoginNotExistIdException(String data) {
+        if(data == "id") {
+            System.out.println("Not Exist Id");
+            return;
+        }
+        else if(data == "pw") {
+            System.out.println("Not Exist Pw");
+            return;
+        }
+        return;
+    }
 }
