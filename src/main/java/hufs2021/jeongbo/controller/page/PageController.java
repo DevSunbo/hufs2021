4a42dc73eab1c6ad0ba46c2fc6971cf40d296f40
DevSunbo <cowwhite1008@gmail.com>
2021-01-24 02:13:13 +0900

    index page(all permit) & mypage(user authorization)

--- src/main/java/hufs2021/jeongbo/controller/page/PageController.java
+++ src/main/java/hufs2021/jeongbo/controller/page/PageController.java
@@ -1,2 +1,21 @@
-package hufs2021.jeongbo.controller.page;public class PageController {
+package hufs2021.jeongbo.controller.page;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+//@RequestMapping("templates")
+public class PageController {
+
+    @GetMapping("/index")
+    public ModelAndView index(){
+        return new ModelAndView("/index");
+    }
+
+    @GetMapping("mypage")
+    public ModelAndView myPage(){
+        return new ModelAndView("/mypage");
+    }
 }
