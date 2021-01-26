16a0f6223d42ffed668ea15e7e2a9f964fd684c4
DevSunbo <cowwhite1008@gmail.com>
2021-01-26 19:47:18 +0900

    sout get session

--- src/main/java/hufs2021/jeongbo/controller/page/PageController.java
+++ src/main/java/hufs2021/jeongbo/controller/page/PageController.java
@@ -1,9 +1,14 @@
 package hufs2021.jeongbo.controller.page;
 
+import hufs2021.jeongbo.dto.SessionDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @RestController
 //@RequestMapping("templates")
@@ -14,8 +19,22 @@ public class PageController {
         return new ModelAndView("/index");
     }
 
-    @GetMapping("mypage")
-    public ModelAndView myPage(){
+    @GetMapping("/mypage")
+    public ModelAndView myPage(HttpServletRequest request, HttpServletResponse response, Object handler){
+        HttpSession httpSession = request.getSession();
+        System.out.println(httpSession);
+        SessionDto sessionItem = (SessionDto) httpSession.getAttribute("USER");
+        System.out.println(sessionItem);
         return new ModelAndView("/mypage");
+    }
+
+    @GetMapping("/mypage/changepassword")
+    public ModelAndView changePw(){
+        return new ModelAndView("/changepassword");
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login(){
+        return new ModelAndView("/login");
     }
 }
