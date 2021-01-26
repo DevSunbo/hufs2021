f76d45d0f242fd151cd15047e940cacf5818d503
DevSunbo <cowwhite1008@gmail.com>
2021-01-26 19:45:14 +0900

    add annotation

--- src/main/java/hufs2021/jeongbo/dto/LoginDto.java
+++ src/main/java/hufs2021/jeongbo/dto/LoginDto.java
@@ -1,10 +1,14 @@
 package hufs2021.jeongbo.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginDto {
     Integer sid;
 
