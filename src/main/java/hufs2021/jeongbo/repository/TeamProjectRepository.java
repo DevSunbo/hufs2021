86b5ab73fdf27e788ca525803a84d2c3a4089922
DevSunbo <cowwhite1008@gmail.com>
2021-02-03 02:15:31 +0900

    make findTop

--- src/main/java/hufs2021/jeongbo/repository/TeamProjectRepository.java
+++ src/main/java/hufs2021/jeongbo/repository/TeamProjectRepository.java
@@ -4,6 +4,9 @@ import hufs2021.jeongbo.model.entity.TeamProject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TeamProjectRepository extends JpaRepository<TeamProject, Integer> {
+    public List<TeamProject> findTop20ByOrderByProjectNumberDesc();
 }
