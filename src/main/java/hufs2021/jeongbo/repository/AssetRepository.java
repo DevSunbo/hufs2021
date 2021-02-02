c6a7a557b2e44b9f5068e8aec56e9039b9d7cc2a
DevSunbo <cowwhite1008@gmail.com>
2021-02-03 01:26:34 +0900

    make findby20TopByOrderByDesc

--- src/main/java/hufs2021/jeongbo/repository/AssetRepository.java
+++ src/main/java/hufs2021/jeongbo/repository/AssetRepository.java
@@ -4,6 +4,9 @@ import hufs2021.jeongbo.model.entity.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AssetRepository extends JpaRepository<Asset, Integer> {
+    public List<Asset> findTop20ByOrderByAssetIdDesc();
 }
--- src/main/java/hufs2021/jeongbo/repository/QnaRepository.java
+++ src/main/java/hufs2021/jeongbo/repository/QnaRepository.java
@@ -5,6 +5,9 @@ import hufs2021.jeongbo.model.key.QnaId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QnaRepository extends JpaRepository<Qna, QnaId> {
+    public List<Qna> findTop20ByOrderByQaNumberDesc();
 }
