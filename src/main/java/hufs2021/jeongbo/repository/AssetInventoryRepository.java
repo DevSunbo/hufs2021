0db96f14db555d110ed4e249dfcd61099ea278ab
DevSunbo <cowwhite1008@gmail.com>
2021-02-03 19:47:18 +0900

    make find jpa in repository

--- src/main/java/hufs2021/jeongbo/repository/AssetInventoryRepository.java
+++ src/main/java/hufs2021/jeongbo/repository/AssetInventoryRepository.java
@@ -3,5 +3,8 @@ package hufs2021.jeongbo.repository;
 import hufs2021.jeongbo.model.entity.AssetInventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AssetInventoryRepository extends JpaRepository<AssetInventory, Integer> {
+    public  List<AssetInventory> findByCaNumber(Integer caNumber);
 }
--- src/main/java/hufs2021/jeongbo/repository/CategoryRepository.java
+++ src/main/java/hufs2021/jeongbo/repository/CategoryRepository.java
@@ -3,5 +3,11 @@ package hufs2021.jeongbo.repository;
 import hufs2021.jeongbo.model.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    public List<Category> findByCaRental(String caRental);
+    public List<Category> findByCaKind(String caKind);
+    public List<Category> findByCaName(String caName);
+
 }
