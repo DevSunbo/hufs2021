149d5c61c86a7de1287133e3345629bc866ba88d
DevSunbo <cowwhite1008@gmail.com>
2021-02-03 19:47:01 +0900

    Asset page controller -ing

--- src/main/java/hufs2021/jeongbo/controller/api/AssetApiController.java
+++ src/main/java/hufs2021/jeongbo/controller/api/AssetApiController.java
@@ -1,2 +1,39 @@
-package hufs2021.jeongbo.controller.api;public class AssetApiController {
+package hufs2021.jeongbo.controller.api;
+
+import hufs2021.jeongbo.model.dto.AssetDto;
+import hufs2021.jeongbo.model.entity.Asset;
+import hufs2021.jeongbo.model.entity.AssetInventory;
+import hufs2021.jeongbo.model.entity.Category;
+import hufs2021.jeongbo.repository.AssetInventoryRepository;
+import hufs2021.jeongbo.repository.AssetRepository;
+import hufs2021.jeongbo.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/asset")
+public class AssetApiController {
+    @Autowired
+    AssetRepository assetRepository;
+
+    @Autowired
+    AssetInventoryRepository assetInventoryRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    AssetDto assetDto;
+
+    List<Asset> assetList;
+    List<AssetInventory> assetInventorieList;
+    List<Category> categoryList;
+
+    public void read(){
+        //assetList = assetRepository.findAll();
+        //assetInventorieList = assetInventoryRepository.findAll();
+    }
+
 }
