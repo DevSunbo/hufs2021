f750fd7090ebdec687f72b03cd21893775d75d0c
kimcoyg <kjhkjh9753@naver.com>
2021-02-09 21:24:59 +0900

    팀프로젝트 조회 하는 중

--- src/main/resources/frontend/src/App.js
+++ src/main/resources/frontend/src/App.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom';
 import Home from './page/Home';
 import Login from './page/Login';
 import Join from './page/Join';
+import TeamSearch from './page/TeamProjectSearch';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" component={Home} exact={true} />
       <Route path="/login" component={Login}/>
       <Route path="/join" component={Join}/>
+      <Route path="/team-project" component={TeamSearch} exact={true}/>
     </>
   );
 }
---
+++ src/main/resources/frontend/src/page/TeamProjectSearch.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Route, Link} from 'react-router-dom';
+import {Container, Row, Col} from 'react-bootstrap';
+import Header from '../components/Header';
+
+const TeamFont1 = styled.div`
+    font-size: 30px;
+    margin-top: 10px;
+    font-weight: bold;
+`;
+
+const TeamFont2 = styled.div`
+    font-size: 25px;
+    font-weight: normal;
+    margin-top: 40px;
+    vertical-align: middle;
+`;
+
+const TeamFont3 = styled.div`
+    height: 500px;
+`;
+
+const TeamSearch = () => {
+    return (
+        <>
+            <Header />
+            <Container className="mt-5">
+                <Row>
+                    <Col md={{span: 3}}>
+                        <TeamFont3 className="border text-center">
+                            <TeamFont1>팀 프로젝트</TeamFont1>
+                            <TeamFont2 className="align-self-center">
+                                <div className="text-danger">조회</div>
+                                <div className="mt-3">등록</div>
+                            </TeamFont2>
+                        </TeamFont3>
+                    </Col>
+                    <Col className="border">
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+}
+
+export default TeamSearch;
