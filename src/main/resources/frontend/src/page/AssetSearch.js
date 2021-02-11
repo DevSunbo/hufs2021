1465b62bda7e9e723c5ef7bd7a6159cd4f2f99b1
DevSunbo <cowwhite1008@gmail.com>
2021-02-12 00:06:01 +0900

    set component

--- src/main/resources/frontend/src/page/AssetSearch.js
+++ src/main/resources/frontend/src/page/AssetSearch.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Route, Link} from 'react-router-dom';
-import {Container, Row, Col} from 'react-bootstrap';
-import Header from '../components/Header';
+import {Container, Row, Col, Form, Button} from 'react-bootstrap';
+import Header from "../components/Header";
+
+const AssetSearch = () => {
+    return (
+        <>
+            <Header />A
+            AssetSearch
+        </>
+    )
+}
---
+++ src/main/resources/frontend/src/page/myPage/TeamProjectApplication.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Container, Row, Col, Form, Button} from 'react-bootstrap';
+import Header from "../components/Header";
+
+const TeamProjectApplication = () => {
+    return (
+        <>
+            <Header />
+            TeamProjectApplication
+        </>
+    )
+}
