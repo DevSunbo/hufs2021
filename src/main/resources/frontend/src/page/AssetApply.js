709743d493668ae6de068b71e13d2d90ccf7de5e
DevSunbo <cowwhite1008@gmail.com>
2021-02-13 02:10:14 +0900

    make baseline

--- src/main/resources/frontend/src/page/AssetApply.js
+++ src/main/resources/frontend/src/page/AssetApply.js
@@ -2,12 +2,75 @@ import React from 'react';
 import styled from 'styled-components';
 import {Container, Row, Col, Form, Button} from 'react-bootstrap';
 import Header from "../components/Header";
+import {Link} from "react-router-dom";
+
+const ApplyFont = styled.div`
+    font-size: 30px;
+`;
 
 const AssetApply = () => {
     return (
         <>
-            <Header />
-            AssetApply
+            <Header/>
+            <Container className="mt-5">
+                <Row>
+                    <ApplyFont>기자재신청</ApplyFont>
+                </Row>
+                <Row>
+                    <Col>
+                        <div className="border text-center">
+                            <div>기자재</div>
+                            <div className="align-self-center">
+                                <div className="text-danger"><Link to="/asset">조회</Link></div>
+                                <div className="mt-3"><Link to="/asset/apply">신청</Link></div>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+
+                        <div className="mt-2 border p-4">
+                            <Form>
+                                <Form.Group controlId="caRent">
+                                    <Row className="border">
+                                        <Form.Label>분류</Form.Label>
+                                        <Col>
+                                            <Form.Control plaintext readOnly defaultValue="대여가능"/>
+                                        </Col>
+                                        <Col>
+                                            <Form.Control plaintext readOnly defaultValue="종류"/>
+                                        </Col>
+                                        <Col>
+                                            <Form.Control plaintext readOnly defaultValue="이름"/>
+                                        </Col>
+                                    </Row>
+                                </Form.Group>
+                                <Form.Group controlId="restAsset">
+                                    <Row className="border">
+                                        <Form.Label>남은재고</Form.Label>
+                                        <Col>
+                                            <Form.Control plaintext readOnly defaultValue="n개"/>
+                                        </Col>
+                                    </Row>
+                                </Form.Group>
+                                <Form.Group controlId="endDate">
+                                    <Row className="border">
+                                        <Form.Label>반납일</Form.Label>
+                                        <Col>
+                                            <Form.Control plaintext readOnly defaultValue="2021/02/13"/>
+                                        </Col>
+                                    </Row>
+                                </Form.Group>
+                                <Form.Group controlId="email" >
+                                    <Form.Label>주의사항</Form.Label>
+                                    <Form.Control  className="border" plaintext readOnly defaultValue="주의 사항 내용"/>
+                                </Form.Group>
+
+                                <Button type="submit" className="text-center">신청</Button>
+                            </Form>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
--- src/main/resources/frontend/src/page/AssetSearch.js
+++ src/main/resources/frontend/src/page/AssetSearch.js
@@ -3,6 +3,7 @@ import Select from 'react-select';
 import styled from 'styled-components';
 import {Container, Row, Col, Form, Button, Dropdown, FormControl, ListGroup} from 'react-bootstrap';
 import Header from "../components/Header";
+import {Link} from "react-router-dom";
 
 const groupStyles = {
     display: 'flex',
@@ -52,54 +53,67 @@ const AssetSearch = () => {
             <Header/>
             <Container>
                 <div><h2>기자재 조회</h2></div>
+
+
                 <Row>
-                    <Col md={{span: 6, offset: 3}} className="mt-5">
-                        <div className="mt-2 border p-4">
-                            <form action="asset">
-                                <div>
-                                    <div className="col-md-3">
+                    <Col md={{span: 6}}>
+                        <div className="border text-center">
+                            <div>기자재</div>
+                            <div className="align-self-center">
+                                <div className="text-danger"><Link to="/asset">조회</Link></div>
+                                <div className="mt-3"><Link to="/asset/apply">신청</Link></div>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col>
+                    <div className="mt-2 border p-4">
+                        <form action="asset">
+                            <Row>
+                                <Col>
                                     <Select
                                         defaultValue={{value: '', label: '대여여부'}}
                                         options={rental}
                                         formatGroupLabel={formatGroupLabel}
                                     />
-                                    </div>
-                                    <div className="col-md-3">
+                                </Col>
+                                <Col>
                                     <Select
                                         defaultValue={{value: '', label: '종류'}}
                                         options={kind}
                                         formatGroupLabel={formatGroupLabel}
                                     />
-                                    </div>
-                                    <div className="col-md-3">
+                                </Col>
+                                <Col>
                                     <Select
                                         defaultValue={{value: '', label: '이름'}}
                                         options={name}
                                         formatGroupLabel={formatGroupLabel}
                                     />
-                                    </div>
-                                </div>
+                                </Col>
                                 <input type="submit" value="검색"/>
-                            </form>
-                        </div>
+                            </Row>
+                        </form>
+                    </div>
                     </Col>
                 </Row>
+                <Row>
                 <div>
-                    //TODO: 2021.02.12 새로운 jsdㅔ ListGroup Data를 받는 컴포넌트 만들기 - sunbo
-                <ListGroup horizontal className="mt-3">
-                    <ListGroup.Item>1</ListGroup.Item>
-                    <ListGroup.Item>2</ListGroup.Item>
-                    <ListGroup.Item>3</ListGroup.Item>
-                    <ListGroup.Item>4</ListGroup.Item>
-                    <ListGroup.Item>5</ListGroup.Item>
-                    <ListGroup.Item>6</ListGroup.Item>
-                    <ListGroup.Item></ListGroup.Item>
-                    <ListGroup.Item></ListGroup.Item>
-                    <ListGroup.Item></ListGroup.Item>
-                    <ListGroup.Item></ListGroup.Item>
-                    <ListGroup.Item></ListGroup.Item>
-                </ListGroup>
+                    {/*//TODO: 2021.02.12 새로운 jsdㅔ ListGroup Data를 받는 컴포넌트 만들기 - sunbo*/}
+                    <ListGroup horizontal className="mt-3">
+                        <ListGroup.Item>1</ListGroup.Item>
+                        <ListGroup.Item>2</ListGroup.Item>
+                        <ListGroup.Item>3</ListGroup.Item>
+                        <ListGroup.Item>4</ListGroup.Item>
+                        <ListGroup.Item>5</ListGroup.Item>
+                        <ListGroup.Item>6</ListGroup.Item>
+                        <ListGroup.Item></ListGroup.Item>
+                        <ListGroup.Item></ListGroup.Item>
+                        <ListGroup.Item></ListGroup.Item>
+                        <ListGroup.Item></ListGroup.Item>
+                        <ListGroup.Item></ListGroup.Item>
+                    </ListGroup>
                 </div>
+                </Row>
             </Container>
         </>
     );
