71d607d27b4f627c7a9e0bbc50fcecda1bc95b2b
DevSunbo <cowwhite1008@gmail.com>
2021-02-12 22:34:02 +0900

    make basic bone

--- src/main/resources/frontend/src/page/AssetSearch.js
+++ src/main/resources/frontend/src/page/AssetSearch.js
@@ -1,21 +1,109 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Select from 'react-select';
 import styled from 'styled-components';
-import {Container, Row, Col, Form, Button} from 'react-bootstrap';
+import {Container, Row, Col, Form, Button, Dropdown, FormControl, ListGroup} from 'react-bootstrap';
 import Header from "../components/Header";
+
+const groupStyles = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+};
+const groupBadgeStyles = {
+    backgroundColor: '#EBECF0',
+    borderRadius: '2em',
+    color: '#172B4D',
+    display: 'inline-block',
+    fontSize: 12,
+    fontWeight: 'normal',
+    lineHeight: '1',
+    minWidth: 1,
+    padding: '0.16666666666667em 0.5em',
+    textAlign: 'center',
+};
+
+const formatGroupLabel = data => (
+    <div style={groupStyles}>
+        <span>{data.label}</span>
+        <span style={groupBadgeStyles}>{data.options.length}</span>
+    </div>
+);
+//TODO: 2021.02.12 각 드롭다운에 조건을 걸어서 대여가능에 보이는것과 아닌 것 구분 & 앞에 선택하지 않으면 데이터 안보이게 하기 - sunbo
+const rental = [{value: 'canRent', label: '대여가능'},
+    {value: 'cannotRent', label: '대여불가능'},
+];
+
+const kind = [
+    {value: 'notebook', label: '노트북'},
+    {value: 'tablet', label: '태블릿'},
+    {value: 'gift', label: '증점품'},
+    {value: 'bipum', label: '비품'}
+]
+
+const name = [
+    {value: 'leno', label: '레노버'},
+    {value: 'macair', label: '맥북에어'},
+]
+
 
 const AssetSearch = () => {
     return (
         <>
-            <Header />A
+            <Header/>
             <Container>
+                <div><h2>기자재 조회</h2></div>
+                <Row>
+                    <Col md={{span: 6, offset: 3}} className="mt-5">
+                        <div className="mt-2 border p-4">
+                            <form action="asset">
+                                <div>
+                                    <div className="col-md-3">
+                                    <Select
+                                        defaultValue={{value: '', label: '대여여부'}}
+                                        options={rental}
+                                        formatGroupLabel={formatGroupLabel}
+                                    />
+                                    </div>
+                                    <div className="col-md-3">
+                                    <Select
+                                        defaultValue={{value: '', label: '종류'}}
+                                        options={kind}
+                                        formatGroupLabel={formatGroupLabel}
+                                    />
+                                    </div>
+                                    <div className="col-md-3">
+                                    <Select
+                                        defaultValue={{value: '', label: '이름'}}
+                                        options={name}
+                                        formatGroupLabel={formatGroupLabel}
+                                    />
+                                    </div>
+                                </div>
+                                <input type="submit" value="검색"/>
+                            </form>
+                        </div>
+                    </Col>
+                </Row>
                 <div>
-                    <form action="asset">
-                        <div></div>
-                    </form>
+                    //TODO: 2021.02.12 새로운 jsdㅔ ListGroup Data를 받는 컴포넌트 만들기 - sunbo
+                <ListGroup horizontal className="mt-3">
+                    <ListGroup.Item>1</ListGroup.Item>
+                    <ListGroup.Item>2</ListGroup.Item>
+                    <ListGroup.Item>3</ListGroup.Item>
+                    <ListGroup.Item>4</ListGroup.Item>
+                    <ListGroup.Item>5</ListGroup.Item>
+                    <ListGroup.Item>6</ListGroup.Item>
+                    <ListGroup.Item></ListGroup.Item>
+                    <ListGroup.Item></ListGroup.Item>
+                    <ListGroup.Item></ListGroup.Item>
+                    <ListGroup.Item></ListGroup.Item>
+                    <ListGroup.Item></ListGroup.Item>
+                </ListGroup>
                 </div>
             </Container>
         </>
-    )
+    );
 }
 
 export default AssetSearch;
+
